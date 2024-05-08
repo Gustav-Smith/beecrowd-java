11a9db8ef2d1880ab98edee25c85e5bb973de617
Gustav-Smith <gustavojeanpt@gmail.com>
2024-05-07 21:01:58 -0300

    Bee1002

--- src/Bee1002.java
+++ src/Bee1002.java
@@ -1,2 +1,10 @@
+import java.util.*;
+
 public class Bee1002 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        double r = scan.nextDouble();
+        double area = 3.14159 * r * r;
+        System.out.printf("A=%.4f\n", area);
+    }
 }
